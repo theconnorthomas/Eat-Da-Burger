9ce36ec4e7cbed7ec8cadb0e841c746a3716a42d
Connor Thomas <connor.ian.thomas@gmail.com>
2018-05-30 22:28:18 -0500

    split schema and seed

--- schema.sql
+++ schema.sql
@@ -12,8 +12,3 @@ CREATE TABLE burgers
   	dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   	PRIMARY KEY(id)
 );
-
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Pickle Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mustard Whapper', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
---
+++ seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Pickle Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mustard Whapper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
