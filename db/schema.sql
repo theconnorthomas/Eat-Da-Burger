57f04c350668dc3042e4443e047a2154faee61ee
Connor Thomas <connor.ian.thomas@gmail.com>
2018-07-01 17:38:20 -0500

    stuff

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(100) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+  	PRIMARY KEY(id)
+);
