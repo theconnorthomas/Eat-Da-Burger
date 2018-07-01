5fa0e314d2adaeaca996edd0acebe3295b7856d4
Connor Thomas <connor.ian.thomas@gmail.com>
2018-07-01 17:27:51 -0500

    stuff

--- db/seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Pickle Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mustard Whapper', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
